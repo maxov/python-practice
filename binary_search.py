17148924c45022a1a528829933517121d21613f9
Max Ovsiankin <max@ovsankin.com>
2016-02-16 18:13:27 -0800

    We can ignore the middle value when it is not equal to the value we're looking for

--- binary_search.py
+++ binary_search.py
@@ -1,15 +1,20 @@
 def binary_search(coll, val):
     def recurse(start, end):
+        if start > end:
+            return -1
         middle = int((start + end) / 2)
         compare = coll[middle]
         if compare > val:
-            return recurse(start, middle)
+            return recurse(start, middle - 1)
         elif compare < val:
-            return recurse(middle, end)
+            return recurse(middle + 1, end)
         else:
             return middle
     return recurse(0, len(coll) - 1)
 
-my_coll = [1, 3, 6, 7, 8, 10]
+my_coll = [1, 3, 6, 7]
 
-print(binary_search(my_coll, 6))
+assert binary_search(my_coll, 7) == 3
+assert binary_search(my_coll, 1) == 0
+assert binary_search(my_coll, 3) == 1
+assert binary_search(my_coll, 5) == -1
